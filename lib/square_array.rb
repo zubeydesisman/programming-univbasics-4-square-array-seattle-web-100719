babe6c63846994bcc3ad85ad2912d1a62057d225
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-09-29 19:49:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array = []
 
   while counter < array.length do 
-     new_array.push(array[counter] ** 2)
+     new_array << array[counter] ** 2
     counter += 1 
   end
  return new_array
